36d81562d9183f1df1ed41eed817faae79d67ae7
Michelle Cinello <m.cinello@gmail.com>
2017-10-02 17:14:13 -0400

    changed 'banana' in transport_ingredients_to_glass' to ingredient_name

--- episode-04/replicator.rb
+++ episode-04/replicator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -40,7 +42,6 @@ class Replicator
   # and then manually execute each method to ensure
   # it returns what's expect.
   def replicate(recipe)
-
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
@@ -114,7 +115,7 @@ class Replicator
       @enterprise.transporter.energize(
         # Geordi is in a jokey mood and
         # reprograms the replicator
-        @enterprise.pantry.find_ingredient('banana'),
+        @enterprise.pantry.find_ingredient(ingredient_name),
         @enterprise.pantry.shelf,
         glass_inside_replicator.inside
       )
